65ac7bf1609c4b48d3514e256ec4a14e64b2b135
cbolson <chris@cbolson.com>
2023-07-13 22:51:52 +0200

    update - space bar and numeric keys

--- keyboard/scripts.js
+++ keyboard/scripts.js
@@ -12,83 +12,79 @@ const btnNumericAlt = document.querySelector("#btn-numeric-alt");
 const btnLang = document.querySelector("#btn-lang");
 
 btnShiftEN.addEventListener("click", () => {
-    keyboardNormal.classList.toggle("[&_button]:uppercase");
-    keyboardES.classList.toggle("[&_button]:uppercase");
-    btnShiftEN.classList.toggle("active");
-    btnShiftES.classList.toggle("active");
+  keyboardNormal.classList.toggle("[&_button]:uppercase");
+  keyboardES.classList.toggle("[&_button]:uppercase");
+  btnShiftEN.classList.toggle("active");
+  btnShiftES.classList.toggle("active");
 });
 
 btnShiftES.addEventListener("click", () => {
-    keyboardNormal.classList.toggle("[&_button]:uppercase");
-    keyboardES.classList.toggle("[&_button]:uppercase");
-    btnShiftES.classList.toggle("active");
-    btnShiftEN.classList.toggle("active");
+  keyboardNormal.classList.toggle("[&_button]:uppercase");
+  keyboardES.classList.toggle("[&_button]:uppercase");
+  btnShiftES.classList.toggle("active");
+  btnShiftEN.classList.toggle("active");
 });
 
-
-let currentLang = 'en';
+let currentLang = "en";
 btnLang.addEventListener("click", () => {
-    keyboardNumeric.classList.add("hidden");
-    keyboardNumericAlt.classList.add("hidden");
+  keyboardNumeric.classList.add("hidden");
+  keyboardNumericAlt.classList.add("hidden");
 
-    if (currentLang=="en") {
-        keyboardNormal.classList.add("hidden");
-        keyboardES.classList.remove("hidden");
-        currentLang = "es";
-    } else {
-        keyboardES.classList.add("hidden");
-        keyboardNormal.classList.remove("hidden");
-        currentLang = "en";
-    }
-})
+  if (currentLang == "en") {
+    keyboardNormal.classList.add("hidden");
+    keyboardES.classList.remove("hidden");
+    currentLang = "es";
+  } else {
+    keyboardES.classList.add("hidden");
+    keyboardNormal.classList.remove("hidden");
+    currentLang = "en";
+  }
+});
 
 let showNumeric = false;
 btnNumeric.addEventListener("click", (event) => {
-    keyboardES.classList.add("hidden");
-    keyboardNumericAlt.classList.add("hidden");
+  keyboardES.classList.add("hidden");
+  keyboardNumericAlt.classList.add("hidden");
 
-    if (!showNumeric) {
-        keyboardNormal.classList.add("hidden");
-        keyboardNumeric.classList.remove("hidden");
-        btnNumeric.classList.add("active");
-        showNumeric = true;
-    } else {
-        keyboardNumeric.classList.add("hidden");
-        keyboardNormal.classList.remove("hidden");
-
-        btnNumeric.classList.remove("active");
-        showNumeric = false;
-
-    }
-
-})
-
-btnNumericAlt.addEventListener("click", () => {
-    btnNumericBase.classList.toggle("active");
-
+  if (!showNumeric) {
     keyboardNormal.classList.add("hidden");
-    keyboardES.classList.add("hidden");
-    keyboardNumeric.classList.add("hidden");
-
-    keyboardNumericAlt.classList.remove("hidden");
-    
-})
-
-btnNumericBase.addEventListener("click", () => {
-
-    keyboardNormal.classList.add("hidden");
-    keyboardES.classList.add("hidden");
-    keyboardNumericAlt.classList.add("hidden");
-
     keyboardNumeric.classList.remove("hidden");
 
-})
+    // btnNumeric.classList.add("active");
+    btnNumeric.innerText = "abc";
+    showNumeric = true;
+  } else {
+    keyboardNumeric.classList.add("hidden");
+    keyboardNormal.classList.remove("hidden");
+
+    //btnNumeric.classList.remove("active");
+    btnNumeric.innerText = "123";
+    showNumeric = false;
+  }
+});
+
+btnNumericAlt.addEventListener("click", () => {
+  btnNumericBase.classList.toggle("active");
+
+  keyboardNormal.classList.add("hidden");
+  keyboardES.classList.add("hidden");
+  keyboardNumeric.classList.add("hidden");
+
+  keyboardNumericAlt.classList.remove("hidden");
+});
+
+btnNumericBase.addEventListener("click", () => {
+  keyboardNormal.classList.add("hidden");
+  keyboardES.classList.add("hidden");
+  keyboardNumericAlt.classList.add("hidden");
+
+  keyboardNumeric.classList.remove("hidden");
+});
 
 const keys = document.querySelectorAll("button");
-keys.forEach(key => { 
-    const val = key.innerText;
-    key.addEventListener("click", () => { 
-        console.log(val);
-    })
-    
-})
+keys.forEach((key) => {
+  const val = key.innerText;
+  key.addEventListener("click", () => {
+    console.log(val);
+  });
+});
