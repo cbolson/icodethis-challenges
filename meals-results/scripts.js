2fb008c72a17e9601440425504176cdfc139df77
cbolson <chris@cbolson.com>
2023-07-27 21:44:08 +0200

    fixed - filter

--- meals-results/scripts.js
+++ meals-results/scripts.js
@@ -7,12 +7,64 @@ const FILTERS = [
   "gluten-free",
   "meat",
 ];
-const filtersList = document.querySelector("#filters-list");
-const tplItem = document.querySelector("#tpl-item");
+const MEALS = [
+  {
+    id: 1,
+    desc: "Avocado sandwich with boiled egg",
+    time: "15 mins",
+    img: "https://source.unsplash.com/NoczeWSAF_w/200x200",
+    tags: ["vegetarian", "vegan", "dinner", "protein"],
+    bookmark: false,
+  },
+  {
+    id: 2,
+    desc: "Avocado salad with Bacon",
+    time: "25 mins",
+    img: "https://source.unsplash.com/AibVl6o117k/200x200",
+    tags: ["snack", "meat", "protein"],
+    bookmark: false,
+  },
+  {
+    id: 3,
+    desc: "Fresh Avocado salad ",
+    time: "15 mins",
+    img: "https://source.unsplash.com/IGfIGP5ONV0/200x200",
+    tags: ["vegetarian", "vegan", "snack"],
+    bookmark: true,
+  },
+  {
+    id: 4,
+    desc: "Avocado and spinach pesto spagetti",
+    time: "30 mins",
+    img: "https://source.unsplash.com/RTyHbFUVOnQ/200x200",
+    tags: ["vegetarian", "vegan", "dinner", "protein"],
+    bookmark: false,
+  },
+  {
+    id: 5,
+    desc: "Avocado tuna hash",
+    time: "20 mins",
+    img: "https://source.unsplash.com/Xmy0XM1U1rE/200x200",
+    tags: ["meat", "dinner", "protein"],
+    bookmark: false,
+  },
+  {
+    id: 6,
+    desc: "Avocado",
+    time: "1 min",
+    img: "https://source.unsplash.com/cueV_oTVsic/200x200",
+    tags: ["vegetarian", "vegan", "snack", "gluten-free"],
+    bookmark: false,
+  },
+];
 
+// start with no filters
 const filtersApplied = [];
 
 // filtr options list
+const filtersList = document.querySelector("#filters-list");
+const tplItem = document.querySelector("#tpl-item");
+
 function renderFilterList() {
   filtersList.innerHTML = "";
 
@@ -30,7 +82,6 @@ function renderFilterList() {
       radio.addEventListener("change", () => {
         if (radio.checked) {
           filtersApplied.push(filter);
-
           // remove
           radio.closest("label").remove();
         } else {
@@ -79,109 +130,63 @@ function renderFiltersApplied() {
     filteredList.append(item);
   });
 }
-
-const MEALS = [
-  {
-    id: 1,
-    desc: "Avocado sandwich with boiled egg",
-    time: "15 mins",
-    img: "https://source.unsplash.com/NoczeWSAF_w/200x200",
-    tags: ["vegetarian", "vegan", "dinner", "protein"],
-    bookmark: false,
-  },
-  {
-    id: 2,
-    desc: "Avocado salad with Bacon",
-    time: "25 mins",
-    img: "https://source.unsplash.com/AibVl6o117k/200x200",
-    tags: ["snack", "meat", "protein"],
-    bookmark: false,
-  },
-  {
-    id: 3,
-    desc: "Fresh Avocado salad ",
-    time: "15 mins",
-    img: "https://source.unsplash.com/IGfIGP5ONV0/200x200",
-    tags: ["vegetarian", "vegan", "snack"],
-    bookmark: true,
-  },
-  {
-    id: 4,
-    desc: "Avocado and spinach pesto spagetti",
-    time: "30 mins",
-    img: "https://source.unsplash.com/RTyHbFUVOnQ/200x200",
-    tags: ["vegetarian", "vegan", "dinner", "protein"],
-    bookmark: false,
-  },
-  {
-    id: 5,
-    desc: "Avocado tuna hashe",
-    time: "20 mins",
-    img: "https://source.unsplash.com/Xmy0XM1U1rE/200x200",
-    tags: ["meat", "dinner", "protein"],
-    bookmark: false,
-  },
-  {
-    id: 6,
-    desc: "Avocado",
-    time: "1 min",
-    img: "https://source.unsplash.com/cueV_oTVsic/200x200",
-    tags: ["vegetarian", "vegan", "snack", "gluten-free"],
-    bookmark: false,
-  },
-];
-
+// meals
 const tplMeal = document.querySelector("#tpl-meal");
 const mealsList = document.querySelector("#meals-list");
 const resultsCounter = document.querySelector("[num-results]");
 
 function renderMeals() {
+  // reset
   let count = 0;
   mealsList.innerHTML = "";
-  MEALS.forEach((meal) => {
-    let showMeal = true;
 
-    if (filtersApplied.length > 0) {
-      showMeal = false;
-      // only return meals with tags
-      filtersApplied.forEach((filter) => {
-        if (meal.tags.includes(filter)) {
-          showMeal = true;
-        } else {
-          showMeal = false;
-        }
+  //
+  let arr;
+  // filter meals if defined
+  if (filtersApplied.length > 0) {
+    arr = MEALS.filter((item) => {
+      return filtersApplied.every((str) => {
+        return item.tags.includes(str);
       });
-    }
+    });
+  } else {
+    arr = MEALS;
+  }
 
-    if (showMeal) {
-      const item = tplMeal.content.cloneNode(true);
-      item.querySelector("h3").innerText = meal.desc;
-      item.querySelector("p").innerText = meal.time;
-      item.querySelector("img").src = meal.img;
-      const btn = item.querySelector("[bookmark]");
+  arr.forEach((meal) => {
+    const item = tplMeal.content.cloneNode(true);
+    item.querySelector("h3").innerText = meal.desc;
+    item.querySelector("p").innerText = meal.time;
+    item.querySelector("img").src = meal.img;
+    const ul = item.querySelector("ul");
+    // tags
+    meal.tags.forEach((tag) => {
+      const el = document.createElement("li");
+      el.innerText = tag;
+      ul.append(el);
+    });
+    const btn = item.querySelector("[bookmark]");
+    if (meal.bookmark) {
+      btn.classList.add("material-fill-1");
+      btn.classList.add("text-red-600");
+    }
+    btn.addEventListener("click", () => {
       if (meal.bookmark) {
+        // remove
+        meal.bookmark = false;
+        btn.classList.remove("material-fill-1");
+        btn.classList.remove("text-red-600");
+      } else {
+        // add
+        meal.bookmark = true;
         btn.classList.add("material-fill-1");
         btn.classList.add("text-red-600");
       }
-      btn.addEventListener("click", () => {
-        if (meal.bookmark) {
-          // remove
-          meal.bookmark = false;
-          btn.classList.remove("material-fill-1");
-          btn.classList.remove("text-red-600");
-        } else {
-          // add
-          meal.bookmark = true;
-          btn.classList.add("material-fill-1");
-          btn.classList.add("text-red-600");
-        }
-      });
-
-      mealsList.append(item);
-      count++;
-    }
-    resultsCounter.innerText = count;
+    });
+    mealsList.append(item);
+    count++;
   });
+  resultsCounter.innerText = count;
 }
 
 renderMeals();
