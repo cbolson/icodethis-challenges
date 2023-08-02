7c9ef74723a383c03055c6dacdd93d2f2f41487f
cbolson <chris@cbolson.com>
2023-08-02 21:43:35 +0200

    new challenge - big footer 2

---
+++ big-footer-2/scrips.js
@@ -0,0 +1,8 @@
+// utility - prevent link clicks
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    if (link.getAttribute("href") == "#") {
+      e.preventDefault();
+    }
+  });
+});
