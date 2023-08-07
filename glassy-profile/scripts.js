c01f3628bc01eb3760e86ed2ae5ac6c1c99c6191
cbolson <chris@cbolson.com>
2023-08-07 20:25:04 +0200

    new challenge - glassy profile

--- glassy-profile/scripts.js
+++ glassy-profile/scripts.js
@@ -15,22 +15,31 @@ const panelSocial = document.querySelector("#panel-social");
 
 document.querySelector("#btn-data").addEventListener("click", (e) => {
   if (dataVisible) {
-    panelData.style.transform = "translate(0)";
+    panelData.style.transform = "translate(0,-75px)";
+    panelSocial.style.transform = "translate(0,45px)";
+    setTimeout(() => {
+      panelData.style.transform = "translate(0)";
+      panelSocial.style.transform = "translate(0)";
+    }, 300);
+
     dataVisible = false;
     e.target.innerText = "density_large";
   } else {
     panelData.style.transform = "translate(0,-70px)";
+    panelSocial.style.transform = "translate(0,40px)";
+
     dataVisible = true;
     e.target.innerText = "close";
   }
 });
-
+/*
 document.querySelector("#btn-social").addEventListener("click", () => {
-  if (socialVisible) {
-    panelSocial.style.transform = "translate(0)";
-    socialVisible = false;
-  } else {
-    panelSocial.style.transform = "translate(0,40px)";
-    socialVisible = true;
-  }
-});
+    if (socialVisible) {
+        panelSocial.style.transform = "translate(0)";
+        socialVisible = false;
+    } else {
+        panelSocial.style.transform = "translate(0,40px)";
+        socialVisible = true;
+    }
+})
+*/
