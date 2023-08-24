64468bae82c326aba260c1e38a31c9459d99c265
cbolson <chris@cbolson.com>
2023-08-24 22:47:23 +0200

    update - ui kit

--- ui-kit/scripts.js
+++ ui-kit/scripts.js
@@ -1,93 +1,178 @@
 // Add JavaScript code here
 tailwind.config = {
-    theme: {
-        screens: {
-            'xs': '400px',
-            'sm': '600px',
-            'md': '900px',
-            'lg': '1024px',
-            'xl': '1280px',
-            '2xl': '1536px',
-
+  theme: {
+    screens: {
+      xs: "400px",
+      sm: "600px",
+      md: "900px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#222F3E",
         },
-        extend: {
-            fontFamily: {
-                sans: ['Roboto', 'sans-serif'],
-            },
-            colors: {
-                primary: {
-                    DEFAULT: "#222F3E"
-                },
-                secondary: {
-                    DEFAULT: "#17AB80"
-                }
-            }
-        }
-    }
-}
+        secondary: {
+          DEFAULT: "#17AB80",
+        },
+      },
+    },
+  },
+};
 
 // utility - prevent link clicks
-document.querySelectorAll("a").forEach(link => {
-    link.addEventListener("click", (e) => {
-        if (link.getAttribute("href") == "#") {
-            e.preventDefault();
-        }
-    });
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    if (link.getAttribute("href") == "#") {
+      e.preventDefault();
+    }
+  });
 });
 
 const msg = document.querySelector("#msg");
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    msg.style.transform = "translate(0)";
+  e.preventDefault();
+  msg.style.transform = "translate(0)";
 
+  setTimeout(() => {
+    // reset form
+    form.reset();
+
+    // slide up
+    msg.style.transform = "translate(0, -120%)";
+    setTimeout(() => (msg.style.opacity = 0), 200);
+
+    // reset to bottom for next submit
     setTimeout(() => {
-        // reset form
-        form.reset();
+      msg.style.transform = "translate(0, 120%)";
+      setTimeout(() => (msg.style.opacity = 1), 300);
+    }, 600);
+  }, 3000);
+});
 
-        // slide up
-        msg.style.transform = "translate(0, -120%)";
-        setTimeout(() => msg.style.opacity = 0, 200);
+const msg2 = document.querySelector("#msg-2");
+document.querySelector("#btn-follow").addEventListener("click", () => {
+  msg2.style.transform = "translate(0)";
 
-        // reset to bottom for next submit
-        setTimeout(() => {
-            msg.style.transform = "translate(0, 120%)";
-            setTimeout(() => msg.style.opacity = 1, 300);
-        }, 600);
+  setTimeout(() => {
+    // slide up
+    msg2.style.transform = "translate(0, -120%)";
+    setTimeout(() => (msg2.style.opacity = 0), 200);
 
-    }, 3000);
-})
-
-
+    // reset to bottom for next submit
+    setTimeout(() => {
+      msg2.style.transform = "translate(0, 120%)";
+      setTimeout(() => (msg2.style.opacity = 1), 300);
+    }, 600);
+  }, 3000);
+});
 
 // constants
 const months = [];
 
-months["en"] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+months["en"] = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-months["es"] = ["Enero", "Febrero", "Marzo", "Abríl", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+months["es"] = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abríl",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
 
-months["fr"] = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "auot", "septembre", "octobre", "novembre", "décembre"];
+months["fr"] = [
+  "janvier",
+  "février",
+  "mars",
+  "avril",
+  "mai",
+  "juin",
+  "juillet",
+  "auot",
+  "septembre",
+  "octobre",
+  "novembre",
+  "décembre",
+];
 
-months["de"] = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
+months["de"] = [
+  "Januar",
+  "Februar",
+  "März",
+  "April",
+  "Mai",
+  "Juni",
+  "Juli",
+  "August",
+  "September",
+  "Oktober",
+  "November",
+  "Dezember",
+];
 
-months["it"] = ["gennaio", "febbraio", "Marzo", "aprile", "maggio", "giugno", "luglio", "Agosto", "settembre", "ottobre", "novembre", "dicembre"];
+months["it"] = [
+  "gennaio",
+  "febbraio",
+  "Marzo",
+  "aprile",
+  "maggio",
+  "giugno",
+  "luglio",
+  "Agosto",
+  "settembre",
+  "ottobre",
+  "novembre",
+  "dicembre",
+];
 
 const days = [];
-days["en"] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+days["en"] = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 days["es"] = ["Do", "Lu", "Ma", "X", "Ju", "Vi", "Sa"];
 days["fr"] = ["Di", "Lu", "Ma", "Mi", "Ji", "Ve", "Sa"];
 days["de"] = ["So", "Mo", "Si", "Mi", "Do", "Fr", "So"];
 days["it"] = ["Do", "Lu", "Ma", "Me", "Gi", "Ve", "Sa"];
 
-let lang = 'en';
+let lang = "en";
 
 // selectors
 const daysTag = document.querySelector("[data-dates]");
 const currentDate = document.querySelector("[data-month]");
 const btnsnNav = document.querySelectorAll("[cal-nav]");
-
 
 // get current date, year and month
 let date = new Date();
@@ -98,66 +183,68 @@ let currMonth = date.getMonth();
 document.querySelector("[date-today]").innerText = date.getDate();
 document.querySelector("[date-weekday]").innerText = days[lang][date.getDay()];
 
-
-//const today = 
+//const today =
 
 (renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
-    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
-    let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay();
-    let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
+  let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+  let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay();
+  let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
 
-    let dateEl = "";
-    // fill previous month days to 1st date of current month
-    for (let i = firstDayofMonth; i > 0; i--) {
-        dateEl += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-    }
+  let dateEl = "";
+  // fill previous month days to 1st date of current month
+  for (let i = firstDayofMonth; i > 0; i--) {
+    dateEl += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+  }
 
-    // current month
-    for (let i = 1; i <= lastDateofMonth; i++) {
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "today" : "";
-        dateEl += `<li class="${isToday}" data-date="">${i}</li>`;
-    }
+  // current month
+  for (let i = 1; i <= lastDateofMonth; i++) {
+    let isToday =
+      i === date.getDate() &&
+      currMonth === new Date().getMonth() &&
+      currYear === new Date().getFullYear()
+        ? "today"
+        : "";
+    dateEl += `<li class="${isToday}" data-date="">${i}</li>`;
+  }
 
-    // add days to complete week A
-    for (let i = lastDayofMonth; i < 6; i++) {
-        dateEl += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
-    }
+  // add days to complete week A
+  for (let i = lastDayofMonth; i < 6; i++) {
+    dateEl += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+  }
 
-
-    // add dates to calendar
-    //console.log(months[lang]);
-    currentDate.innerText = `${months[lang][currMonth]} ${currYear}`;
-    daysTag.innerHTML = dateEl;
+  // add dates to calendar
+  //console.log(months[lang]);
+  currentDate.innerText = `${months[lang][currMonth]} ${currYear}`;
+  daysTag.innerHTML = dateEl;
 })();
 
-
 // calendar month nav buttons
-btnsnNav.forEach(btn => {
-    const direction = btn.getAttribute("cal-nav");
+btnsnNav.forEach((btn) => {
+  const direction = btn.getAttribute("cal-nav");
 
-    btn.addEventListener("click", () => {
-        currMonth = direction === "prev" ? currMonth - 1 : currMonth + 1;
-        if (currMonth < 0 || currMonth > 11) {
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear();
-            currMonth = date.getMonth();
-        } else {
-            date = new Date();
-        }
-        renderCalendar();
-    });
+  btn.addEventListener("click", () => {
+    currMonth = direction === "prev" ? currMonth - 1 : currMonth + 1;
+    if (currMonth < 0 || currMonth > 11) {
+      date = new Date(currYear, currMonth, new Date().getDate());
+      currYear = date.getFullYear();
+      currMonth = date.getMonth();
+    } else {
+      date = new Date();
+    }
+    renderCalendar();
+  });
 });
 
 // day titles
 const weekDays = document.querySelector("[data-weekdays]");
 (renderWeekdays = () => {
-    weekDays.innerHTML = '';
-    days[lang].forEach(day => {
-        const d = document.createElement("li");
-        d.innerText = day.substring(0, 3);
-        weekDays.append(d);
-    })
+  weekDays.innerHTML = "";
+  days[lang].forEach((day) => {
+    const d = document.createElement("li");
+    d.innerText = day.substring(0, 3);
+    weekDays.append(d);
+  });
 })();
 /*
 const langBtns = document.querySelectorAll("[lang-choose]");
