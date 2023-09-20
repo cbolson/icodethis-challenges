e9b804ce190e4862e1582547b93ee8c40092f401
cbolson <chris@cbolson.com>
2023-09-20 20:08:47 +0200

    new challenge - pricing 3

---
+++ pricing-3/scripts.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Mooli", "sans-serif"],
+      },
+      colors: {
+        light: {
+          DEFAULT: "#C9FA24",
+        },
+        startup: {
+          DEFAULT: "#22FFBB",
+        },
+        bigboy: {
+          DEFAULT: "#31D9FD",
+        },
+      },
+    },
+  },
+};
