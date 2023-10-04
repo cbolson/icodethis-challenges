c3ec5bfd169859fb1fd96af02d2c8fa8af585e03
cbolson <chris@cbolson.com>
2023-10-04 20:10:53 +0200

    new challenge - inbox list

---
+++ inbox-list/scripts.js
@@ -0,0 +1,461 @@
+
+
+
+const NEW_MESSGES = [
+  {
+    date: new Date(),
+    username: "Chris",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/888e7b16-fa69-4576-9a45-d8f4fe6f6687.png",
+    text: "🥳 Celebrating my 150th challenge! 🥳",
+    fav: false,
+  },
+  {
+    date: "2023-10-03T14:52:00",
+    username: "dp4k",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/8fbf521c-48d4-4801-82af-64dd6b606293.png",
+    text: `Chris, Congratulations on your 150th submission. Your submissions consistently raise the bar, and really appreciate the insightful ideas you share. 🎉🎉It’s always the animations that amaze me the most!`,
+    fav: true,
+  },
+  {
+    date: "2023-10-03T14:38:00",
+    username: "iMuhammad",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/a20a421f-fc09-4773-96e1-91ae719edb2b.jpg",
+    text: `It’s always the animations that amaze me the most!`,
+    fav: true,
+  },
+  {
+    date: "2023-10-03T13:30:00",
+    username: "Alexander",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/cb43889b-9895-4b6e-a16a-d402ceda7c94.jpg",
+    text: `Awesome job as always.Keep up the good work`,
+    fav: true,
+  },
+
+  {
+    date: "2023-10-01T10:30:00",
+    username: "Kamil",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/3c467dfe-3c18-4d4a-9387-70cbe4429f11.png",
+    text: "Great job @Chris, you even added something extra! 😎 ",
+    fav: true,
+  },
+  {
+    date: "2023-09-19T12:00:00",
+    username: "FlorinPop17",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/b31bde00-b6db-48e5-9798-1c0f053360d4.jpg",
+    text: "Talking about going the extra mile... This is golden! ",
+    fav: false,
+  },
+  {
+    date: "2023-09-24T21:30:00",
+    username: "AatifSohel",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/0979f919-d84a-481a-8be0-832ed4ffb7af.png",
+    text: "This looks amazing 🤩",
+    fav: true,
+  },
+  {
+    date: "2023-10-02T10:00:00",
+    username: "iMuhammad",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/a20a421f-fc09-4773-96e1-91ae719edb2b.jpg",
+    text: "Looks like you're working on something in js. Can't wait see what the final version of this looks like😄",
+    fav: true,
+  },
+  {
+    date: "2023-10-02T13:15:00",
+    username: "Rohit",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/e7026400-3b15-45d5-aef2-0994d40eae99.jpg",
+    text: `AMAZING!!! @Chris comes here every day to give us the best submission. Awesome work ❤️`,
+    fav: true,
+  },
+  {
+    date: "2023-09-28T15:00:00",
+    username: "Peter",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/9779712a-36d0-427f-8c6b-848bd059ed5f.png",
+    text: "Love the zoom in effect on slider item as you hit the dot. Good job 👌",
+    fav: true,
+  },
+  {
+    date: "2023-10-03T10:30:00",
+    username: "Clement",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/139d30a2-9345-4d36-8e84-cc5bb04abd2f.PNG",
+    text: `How did you come up with the animation
+looks great👌!`,
+    fav: true,
+  },
+  {
+    date: "2023-10-02T10:45:00",
+    username: "Aasu_yadav",
+    image:
+      "https://lh3.googleusercontent.com/a/AAcHTteAbaTX-16EQkAw8ZF4jr6sgOGfJEA81B2Unnj62q1zoNE=s96-c",
+    text: "@chris amazing animation ",
+    fav: true,
+  },
+  {
+    date: "2023-09-29T12:00:00",
+    username: "Bharath",
+    image:
+      "https://lh3.googleusercontent.com/a/AAcHTtenMoO7qVdVfWFQ0G7LnCpTiD3iCBwm12uovbI6JkeNmzIV=s96-c",
+    text: `Nice slider @Chris. I looked into the code and its easy to understand. I will learn and implement this is any future challenges. Thanks.`,
+    fav: true,
+  },
+  {
+    date: "2023-10-01T14:20:00",
+    username: "Charles Acosta",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/96ffa1b5-e96f-4172-8ded-e34bc1a4d17f.jpeg",
+    text: "This is soo smooth <3. Great job!",
+    fav: true,
+  },
+  {
+    date: "2023-09-25T11:00:00",
+    username: "idocod3",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/06c26539-cd28-4686-a696-5f68174d1706.jpg",
+    text: "This is soo smooth <3. Great job!",
+    fav: true,
+  },
+  {
+    date: "2023-10-02T20:02:00",
+    username: "Neluttu",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/ef331e37-43e0-48bc-aa39-425ee8295f95.jpg",
+    text: "love this submission, nice work!",
+    fav: true,
+  },
+  {
+    date: "2023-09-27T14:15:00",
+    username: "Fran",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/3f6a261c-c01f-465f-a9af-6ff3805bce4d.jpg",
+    text: `Amazing job as always.
+I've just noticed that the cursor disappears on the closed challenges`,
+    fav: true,
+  },
+  {
+    date: "2023-09-19T15:30:00",
+    username: "imac_000",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/c997b051-6172-4526-8f82-29a2c33cac7c.jpeg",
+    text: `super cool!! ❤️❤️❤️❤️`,
+    fav: true,
+  },
+  {
+    date: "2023-09-19T11:30:00",
+    username: "Patrick",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/bc4a7b71-ad63-4f9d-b23a-5a860561dd0e.png",
+    text: `Hey, your animation was so slick you inspired try building them myself 💯 You can check out my work to see my 3 different attempts at your animation.👍`,
+    fav: true,
+  },
+  {
+    date: "2023-09-19T17:13:00",
+    username: "Rozvita Lilly Stiglinc",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/bb0f3edf-92cd-47b1-a78a-0ebe90bf86e2.jpg",
+    text: `excellent job like usuall :) `,
+    fav: true,
+  },
+  {
+    date: "2023-10-03T15:52:00",
+    username: "user3322b212",
+    image: "https://avatars.githubusercontent.com/u/100511943?v=4",
+    text: `you inspired me, great work!`,
+    fav: true,
+  },
+  {
+    date: "2023-10-03T16:14:00",
+    username: "Peter",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/9779712a-36d0-427f-8c6b-848bd059ed5f.png",
+    text: `...On a more festive note 🎉🎉: Thanks you so much for inspire us with those incredible animations, transitions and the importance of taking the submissions three of four steps further!! `,
+    fav: true,
+  },
+  {
+    date: "2023-10-03T16:30:00",
+    username: "idocod3",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/06c26539-cd28-4686-a696-5f68174d1706.jpg",
+    text: `Outstanding work 🙌🙌`,
+    fav: true,
+  },
+  {
+    date: "2023-10-03T17:16:30",
+    username: "Rohit",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/e7026400-3b15-45d5-aef2-0994d40eae99.jpg",
+    text: `Congratulations @Chris on your 150th submission. Keep inspiring us ❤️.`,
+    fav: true,
+  },
+  {
+    date: "2023-10-03T17:23:29",
+    username: "FlorinPop17",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/b31bde00-b6db-48e5-9798-1c0f053360d4.jpg",
+    text: `Congrats Chris on the huge milestone!!! 🔥`,
+    fav: true,
+  },
+  {
+    date: "2023-10-03T17:31:00",
+    username: "Adrian",
+    image:
+      "https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/6d9761f7-21fd-49dc-b8d6-f7e17c2b6234.jpg",
+    text: `Congratulations, Chris! 🥳
+We are fortunate to have a colleague like you! 🤝`,
+    fav: true,
+  },
+  {
+    date: "2023-10-03T18:18:00",
+    username: "Aasu_yadav",
+    image:
+      "https://lh3.googleusercontent.com/a/AAcHTteAbaTX-16EQkAw8ZF4jr6sgOGfJEA81B2Unnj62q1zoNE=s96-c",
+    text: `congratulation @chris and your animations is cool`,
+    fav: true,
+  },
+  {
+    date: "2023-10-04T06:30:00",
+    username: "clevermissfox",
+    image:
+      "https://lh3.googleusercontent.com/a/ACg8ocI-HcizvJdKxntYPu1az_lq1XEhrxUmlI1LtStEKHF3ug=s96-c",
+    text: `Gosh you are an absolute inspiration to n00bs like me!! `,
+    fav: true,
+  },
+  {
+    date: "2023-10-04T09:14:00",
+    username: "wildecodx",
+    image: "https://avatars.githubusercontent.com/u/130340385?v=4",
+    text: `I just try to put my comment and check if my name is on the contacts. I guess it will happen`,
+    fav: true,
+  },
+];
+/*
+
+
+,
+    {
+        "date": "2023-10-03T16:30:00",
+        "username": "",
+        "image": "",
+        "text": ``,
+        fav: true
+    }
+
+,
+    {
+        "date": "2023-10-02T11:30:00",
+        "username": "Adrian",
+        "image": "https://example.com/user2_image.jpg",
+        "text": "So much to do at my new job!",
+        fav: true
+    },
+ {
+        "date": "2023-10-02T16:15:00",
+        "username": "Julia Pelayo",
+        "image": "https://example.com/user2_image.jpg",
+        "text": "Sorry I wasn't able to to reply earlier",
+        fav: true
+    },
+    {
+        "date": "2023-10-02T17:45:00",
+        "username": "Rebecca Simpson",
+        "image": "https://example.com/user1_image.jpg",
+        "text": "What did she say about what had happened?",
+        fav: false
+    },
+    {
+        "date": "2023-10-01T08:20:00",
+        "username": "Alejando Moreno",
+        "image": "https://example.com/user3_image.jpg",
+        "text": "Call me when you get a moment",
+        fav: false
+    },
+    {
+        "date": "2023-10-03T09:05:00",
+        "username": "Pedro Pica Piedra",
+        "image": "https://example.com/user4_image.jpg",
+        "text": "The car has broken down again!",
+        fav: true
+    },
+*/
+
+// selectors
+const panelInfo = document.querySelector("#panel-info");
+const panelMessages = document.querySelector("#panel-messages");
+const btnMessages = document.querySelector("#btn-messages");
+const btnCloseMessages = document.querySelector("#btn-close-messages");
+const tplItem = document.querySelector("#tpl-item");
+const listItems = document.querySelector("#list-messages");
+const searchInput = document.querySelector("#search");
+const btnResetSearch = document.querySelector("#btn-reset");
+const partyIcon = document.querySelector("#icon");
+
+// delay between each list message load
+const MSG_LOAD_DELAY = 200;
+let numToShow = 5;
+
+// show message list
+btnMessages.addEventListener("click", () => {
+  partyIcon.classList.add("scale-0", "opacity-0");
+
+  // separate timeout delays for each element so that it can be fine-tuned
+  setTimeout(() => btnMessages.classList.add("-translate-y-full"), 300);
+  setTimeout(() => (panelInfo.style.width = "600px"), 600);
+  setTimeout(() => (panelInfo.style.height = "540px"), 1000);
+  setTimeout(() => panelMessages.classList.add("-translate-y-[10.5rem]"), 1000);
+  setTimeout(() => btnCloseMessages.classList.remove("translate-x-20"), 2000);
+  setTimeout(() => renderMessages(), 700);
+  // put icon back
+  setTimeout(() => partyIcon.classList.remove("scale-0", "opacity-0"), 2000);
+});
+
+// hide message list
+btnCloseMessages.addEventListener("click", () => {
+  // hide "close" button
+  btnCloseMessages.classList.add("translate-x-20");
+  // separate timeout delays for each element so that it can be fine-tuned
+  setTimeout(
+    () => panelMessages.classList.remove("-translate-y-[10.5rem]"),
+    300
+  );
+  setTimeout(() => btnMessages.classList.remove("-translate-y-full"), 800);
+  setTimeout(() => (panelInfo.style.width = "220px"), 400);
+  setTimeout(() => (panelInfo.style.height = "170px"), 400);
+});
+
+// load messages - this will be extented to accpet a search term and a limit
+function renderMessages(term = "") {
+  listItems.innerHTML = "";
+  let results = [];
+  if (term) {
+    // remove disabled from bytton
+    btnResetSearch.disabled = false;
+
+    // search term
+    results = NEW_MESSGES.filter((x) =>
+      x.username.toLowerCase().includes(term.toLowerCase())
+    );
+    if (results.length < 1) {
+      listItems.innerHTML =
+        '<p class="text-red-500 text-center ">Sorry, you have no contactas with that name</p>';
+      return;
+    }
+  } else {
+    results = orderByDateDescending(NEW_MESSGES);
+  }
+
+  //  console.log(results);
+  let delay = 100;
+  results.forEach(({ fav, username, date, text, image }) => {
+    const clone = tplItem.content.cloneNode(true);
+    const btn = clone.querySelector("button");
+    const imgEl = clone.querySelector("[data-img]");
+
+    // if (fav) clone.querySelector("[data-fav]").classList.add("text-yellow-500", "material-fill-1");
+    clone.querySelector("[data-name]").innerText = username;
+    clone.querySelector("[data-msg]").innerText = text;
+    if (image) imgEl.src = image;
+    else imgEl.remove();
+    clone.querySelector("time").innerText = formatDateToRelativeTime(date);
+
+    // add to DOM
+    listItems.append(clone);
+
+    // reveal each item after incremental delay
+    setTimeout(() => btn.classList.remove("opacity-0"), delay);
+    delay = delay + MSG_LOAD_DELAY;
+  });
+}
+
+// search
+// debounce to prevent calling the apis if the user is still typing
+function debounce(func, delay = 1000) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
+
+const sendDebounce = debounce((term) => {
+  renderMessages(term);
+}, 500);
+
+// search box (with debounce)
+searchInput.addEventListener("keyup", (e) => {
+  const term = e.target.value;
+  sendDebounce(term);
+});
+
+// btn - clear search box and resalts
+
+btnResetSearch.addEventListener("click", () => {
+  btnResetSearch.innerText = "ink_eraser";
+  btnResetSearch.classList.add("translate-x-[-500px]");
+  setTimeout(() => (searchInput.value = ""), 200);
+  setTimeout(
+    () => btnResetSearch.classList.remove("translate-x-[-500px]"),
+    500
+  );
+  setTimeout(() => (btnResetSearch.innerText = "close"), 500);
+  setTimeout(() => renderMessages(), 700);
+  btnResetSearch.disabled = true;
+});
+
+// utility - order by date (newest first)
+function orderByDateDescending(array) {
+  return array.sort(function (a, b) {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateB - dateA; // Compare in descending order
+  });
+}
+
+// utility - format date relative to current (eg 2 hours ago)
+function formatDateToRelativeTime(d) {
+  const inputDate = new Date(d);
+
+  const now = new Date();
+  const timeDifference = now - inputDate;
+
+  // Convert milliseconds to seconds
+  const seconds = Math.floor(timeDifference / 1000);
+
+  if (seconds < 60) {
+    return `${seconds} seconds ago`;
+  } else if (seconds < 3600) {
+    const minutes = Math.floor(seconds / 60);
+    return `${minutes} minutes ago`;
+  } else if (seconds < 86400) {
+    const hours = Math.floor(seconds / 3600);
+    return `${hours} hours ago`;
+  } else {
+    return formatDate(inputDate);
+  }
+}
+
+// futility - format date ()
+function formatDate(date) {
+  const options = {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+  };
+  /*
+      hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+        */
+  const newDate = new Date(date);
+  return newDate.toLocaleString("en-US", options);
+}
