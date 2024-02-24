f99aa24d99d89b85c38332094cd8c893a689809f
cbolson <chris@cbolson.com>
2024-02-24 20:04:21 +0100

    new challenge - course progress

---
+++ course-progress/scripts.js
@@ -0,0 +1,13 @@
+// tailwind config
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        robotoMono: ["Roboto Mono", "monospace"],
+      },
+      colors: {
+        primary: "#CF649A",
+      },
+    },
+  },
+};
