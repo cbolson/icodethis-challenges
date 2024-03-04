e6271c2360abf61672a7fddaebf75a360cfbe38c
cbolson <chris@cbolson.com>
2024-03-04 11:22:39 +0100

    new challenge - give feedback

---
+++ give-feedback/scripts.js
@@ -0,0 +1,12 @@
+// tailwind config
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Popins", "sans-serif"],
+        merriweather: ["Merriweather", "serif"],
+        comic: ["Comic Neue", "handwriting"],
+      },
+    },
+  },
+};
